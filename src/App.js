81d739cff2c3b8898c42e740560535662c5a0d6b
unknown <ranaivoharitiananomena@gmail.com>
2021-02-10 20:51:14 +0300

    adding button delete #main

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import React, {useRef} from 'react'
 import CanvasDraw from 'react-canvas-draw'
 function App() {
+
+  const firstCanvas = useRef(null)
+
+  const effacer = () =>{
+    firstCanvas.current.clear()
+  }
+
+  const annuler = () =>{
+    firstCanvas.current.undo()
+  }
+
   return (
     <div>
+      <button onClick={effacer}>
+        Effacer
+      </button>
       <CanvasDraw 
        brushRadius={0.3} 
-      hideGrid={true}
-      style={{border:'1px solid #000'}}
+        hideGrid={true}
+        style={{border:'1px solid #000'}}
+        ref={firstCanvas}
       />
     </div>
   );
